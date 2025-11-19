152d6a4c05657c4a46a28cfe6224a74a0656f930
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-11-19 18:31:39 +0200

    font

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -1,6 +1,6 @@
 import { Roboto_Mono } from "next/font/google";
 
-export const robotoMono = Roboto_Mono({
+const robotoMono = Roboto_Mono({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-mono",
